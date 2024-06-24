78a51b8f5b9a55b92a3ca09f20e044eea7595f94
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-06-24 16:16:53 +0700

    chore: change init shcema

--- misc/migrations/000001_init_schema.up.sql
+++ misc/migrations/000001_init_schema.up.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS "users" (
   "id" bigserial PRIMARY KEY,
   "username" varchar NOT NULL,
-  "password" varchar NOT NULL,
+  "email" varchar NOT NULL,
+  "password" varchar,
+  "avatar"  varchar,
+  "is_oauth" boolean DEFAULT false,
+  "is_activated" boolean DEFAULT false,
   "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now()),
   "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now())
 );
