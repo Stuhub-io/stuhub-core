9973bf84bde41ada8b4f133eb6c76a3116eaca48
tonible14012002 <namanh14.work@gmail.com>
2024-12-28 17:34:30 +0700

    feat: add inherit value for page role

--- internal/repository/model/page_roles.gen.go
+++ internal/repository/model/page_roles.gen.go
@@ -13,12 +13,12 @@ const TableNamePageRole = "page_roles"
 // PageRole mapped from table <page_roles>
 type PageRole struct {
 	Pkid      int64     `gorm:"column:pkid;type:bigint;primaryKey;autoIncrement:true" json:"pkid"`
-	PagePkid  int64     `gorm:"column:page_pkid;type:bigint;not null" json:"page_pkid"`
+	PagePkid  int64     `gorm:"column:page_pkid;type:bigint;not null;uniqueIndex:page_roles_page_email_unique_idx,priority:1" json:"page_pkid"`
 	UserPkid  *int64    `gorm:"column:user_pkid;type:bigint" json:"user_pkid"`
 	Role      string    `gorm:"column:role;type:character varying(20);not null;default:viewer" json:"role"`
 	CreatedAt time.Time `gorm:"column:created_at;type:timestamp with time zone;not null;default:now()" json:"created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at;type:timestamp with time zone;not null;default:now()" json:"updated_at"`
-	Email     string    `gorm:"column:email;type:character varying(255);not null" json:"email"`
+	Email     string    `gorm:"column:email;type:character varying(255);not null;uniqueIndex:page_roles_page_email_unique_idx,priority:2" json:"email"`
 }
 
 // TableName PageRole's table name
